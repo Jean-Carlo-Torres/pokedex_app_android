c49b71dbb1e28e94ee0c015a9a11c0945b1131ec
Jean Carlo <jeansanto2002@gmail.com>
2024-05-19 16:30:01 -0300

    Criando a barra do rodapé

---
+++ app/src/main/java/br/com/pokedex/ui/components/FooterBar.kt
@@ -0,0 +1,96 @@
+package br.com.pokedex.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import br.com.pokedex.R
+
+enum class FooterButton(val iconOn: Int, val iconOff: Int, val title: String) {
+    POKEDEX(R.drawable.ic_pokedex_on, R.drawable.ic_pokedex_off, "Pokédex"),
+    FAVORITE(R.drawable.ic_favorite_on, R.drawable.ic_favorite_off, "Favoritos"),
+    USER(R.drawable.ic_user_on, R.drawable.ic_user_off, "Perfil")
+}
+
+@Composable
+fun FooterBar(navController: NavController?) {
+    var selectedButton by remember { mutableStateOf(FooterButton.POKEDEX) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(4.dp),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(72.dp)
+                .background(color = Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                FooterButton.values().forEach { button ->
+                    IconDefault(
+                        image = if (button == selectedButton) button.iconOn else button.iconOff,
+                        title = if (button == selectedButton) button.title else null,
+                        onClick = { selectedButton = button }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun IconDefault(image: Int, title: String?, onClick: () -> Unit) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        IconButton(
+            onClick = onClick,
+        ) {
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(48.dp)
+                    .height(48.dp)
+            )
+        }
+        if (title != null) {
+            Text(text = title)
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun FooterBarPreview() {
+    FooterBar(navController = null)
+}
--- app/src/main/java/br/com/pokedex/ui/screens/ListaPokemonScreen.kt
+++ app/src/main/java/br/com/pokedex/ui/screens/ListaPokemonScreen.kt
@@ -1,57 +1,66 @@
 package br.com.pokedex.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import br.com.pokedex.ui.components.FooterBar
 import br.com.pokedex.ui.components.ListaCardPokemon
 import br.com.pokedex.ui.components.cards.*
 
 @Composable
 fun ListaPokemonScreen(navController: NavController?) {
-    Column(
-        modifier = Modifier
-            .padding(8.dp)
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+                .padding(bottom = 72.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
 
-        val pokemonList = listOf(
-            BulbasaurListData(),
-            IvysaurListData(),
-            VenusaurListData(),
-            CharmanderListData(),
-            CharmeleonListData(),
-            CharizardListData(),
-            SquirtleListData(),
-            WartortleListData(),
-            BlastoiseListData(),
-            BeedrillListData(),
-            PikachuListData(),
-            CleifairyListData(),
-            DugtrioListData(),
-            OnixListData(),
-            LickitungListData(),
-            KoffingListData(),
-            MewListData()
-        )
+            val pokemonList = listOf(
+                BulbasaurListData(),
+                IvysaurListData(),
+                VenusaurListData(),
+                CharmanderListData(),
+                CharmeleonListData(),
+                CharizardListData(),
+                SquirtleListData(),
+                WartortleListData(),
+                BlastoiseListData(),
+                BeedrillListData(),
+                PikachuListData(),
+                CleifairyListData(),
+                DugtrioListData(),
+                OnixListData(),
+                LickitungListData(),
+                KoffingListData(),
+                MewListData()
+            )
 
-        pokemonList.forEachIndexed { index, pokemon ->
-            ListaCardPokemon(pokemon = pokemon, onClick = {
-                navController?.navigate("cardPokemonScreen/$index")
-            })
+            pokemonList.forEachIndexed { index, pokemon ->
+                ListaCardPokemon(pokemon = pokemon, onClick = {
+                    navController?.navigate("cardPokemonScreen/$index")
+                })
+            }
         }
+        FooterBar(navController = navController)
     }
 }
-
 @Preview(showBackground = true)
 @Composable
 private fun ListaPokemonScreenPreview() {
